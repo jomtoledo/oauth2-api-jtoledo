4787e6326d6e7c2fd3e5428f9a57233031d0a591
Jomar Toledo <jomtoledo@gmail.com>
2024-10-02 14:23:28 +1000

    Configure the web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::middleware(['auth:web'])->group(function () {
+    Route::get('/', [CustomerController::class, 'index'])->name('customers'); // Root route
+    Route::get('/customers', [CustomerController::class, 'index']);
 });
