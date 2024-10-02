ce88f7fb1e67d353658c0d3b1058842ec4dd7c2e
Jomar Toledo <jomtoledo@gmail.com>
2024-10-02 14:07:36 +1000

    Created Customer Model

---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $table = 'customers';
+
+    // Define the attributes that are mass assignable
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'age',
+        'dob',
+        'email',
+    ];
+}
